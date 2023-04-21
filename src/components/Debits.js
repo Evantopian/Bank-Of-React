3a895737e55d978c1784a75b92de01f56a16b85a
gnauh <evanacademics@gmail.com>
2023-04-21 19:34:58 -0400

    fixed debit issues

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,32 +1,30 @@
-import { Link } from "react-router-dom";
-import AccountBalance from "./AccountBalance";
+import { Link } from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
-function Debits(props) {
-
+function Debits({ debits, addDebit, accountBalance }) {
   /*
     Function to handle adding a new debit transaction. Parses the 
     input values from the form, calls the addDebit() function passed 
     as a prop to update the parent component's state, and then resets the form.
   */
-  const handleAddDebit = (event) => {
-     event.preventDefault();
-     const description = event.target.elements.description.value;
-     const amount = Number(event.target.elements.amount.value).toFixed(2);
-     props.addDebit(description, amount);
-     event.target.reset();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { description, amount } = e.target.elements;
+    const roundedAmount = Number(amount.value).toFixed(2); 
+    addDebit(description.value, Number(roundedAmount));
+    e.target.reset();
   };
-  
+
   /*
     Function to map over the list of debit transactions 
     passed as a prop and render each one as an <li> element 
     with the transaction's details.
   */  
-    const debitsView = props.debits.map(({ id, amount, description, date }) => (
-      <li key={id}>
-        {amount} {description} {date.slice(0, 10)}
-      </li>
-    ));
-    
+  const debitsView = debits.map(({ id, amount, description, date }) => (
+    <li key={id}>
+      {amount} {description} {date.slice(0, 10)}
+    </li>
+  ));
 
   /*
     The Debits component renders a list of debit transactions 
@@ -38,13 +36,12 @@ function Debits(props) {
     <div>
       <h1>Debits</h1>
       <ul>{debitsView}</ul>
-      <form onSubmit={handleAddDebit}>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="description" />
         <input type="text" name="amount" />
         <button type="submit">Add Debit</button>
       </form>
-
-      <AccountBalance accountBalance={props.accountBalance} />
+      <AccountBalance accountBalance={accountBalance} />
       <br />
       <Link to="/">Return to Home</Link>
     </div>
